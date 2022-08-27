7dc4c121a55c2fb71f97320c61cd4e788e9bbe07
Lucas <lucasdamata01@hotmail.com>
2022-08-27 19:30:28 -0300

    Mais NumPy e comentário
    Utiliza do NumPy para fazer as contas, e mostra no final a media.

--- Ex02-NumPy/main.py
+++ Ex02-NumPy/main.py
@@ -1,15 +1,23 @@
 import numpy as np
 
+#Cria arrays com as informações no txt
 alturas = np.loadtxt(".\\Arquivos\\altura.txt")
 anos = np.loadtxt(".\\Arquivos\\anos.txt")
 
+#Variaveis usadas para coletar as alturas
 cont = 0
-qtd_alturas = 0
-soma_altura = 0
+list_altura = list()
+
+#Pega as alturas com base nos anos
 for ano in anos:
     if(int(ano) > 1998 and int(ano) < 2005):
-        soma_altura += float(alturas[cont])
-        qtd_alturas += 1
+        list_altura.append(alturas[cont])
     cont += 1
 
-media = soma_altura/qtd_alturas
+#Transforma a lista em NumPy.Array
+amostra_alturas = np.array(list_altura)
+
+#Calcula a media
+media = np.mean(amostra_alturas)
+
+print("Media:", round(media, 3),"cm")
